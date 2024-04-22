1b2821f0dff40da95c010444cf2829eaf8dfafed
rjspyk5 <rjspyk5@gmail.com>
2024-04-23 01:28:24 +0600

    fuctionality added for insert coffe details

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ app.use(express.json());
 // connect database
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.omgilvs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -23,24 +21,24 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
+    const coffeCollection = client
+      .db("CoffeShop")
+      .collection("coffeCollection");
+
+    app.post("/coffes", async (req, res) => {
+      const data = req.body;
+      const result = await coffeCollection.insertOne(data);
+      res.send(result);
+    });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
   }
 }
 run().catch(console.dir);
-
-// apis
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
 
 app.listen(port, () => {
   console.log("server is running");
