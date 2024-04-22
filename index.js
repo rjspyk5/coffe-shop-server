fde79189f8213daf671c32b438f091a117836362
rjspyk5 <rjspyk5@gmail.com>
2024-04-22 12:01:14 +0600

    initial setup of server

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// apis
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(port, () => {
+  console.log("server is running");
+});
